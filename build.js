add3a52b41d3d541c17053dd8c3f671e9e0e910e
phothinmg <phothinmg@gmail.com>
2025-04-10 11:58:17 +0630

    new file:   biome.jsonc

--- build.js
+++ build.js
@@ -3,17 +3,17 @@
 import { build } from "lwe8-build";
 
 await (async () => {
-  /**
-   * @type {BuildOptions}
-   */
-  const options = {
-    format: ["esm"],
-    indexFile: {
-      path: "./src/index.ts",
-    },
-    outputDirs: {
-      esm: "./dist",
-    },
-  };
-  await build(options);
+	/**
+	 * @type {BuildOptions}
+	 */
+	const options = {
+		format: ["esm"],
+		indexFile: {
+			path: "./src/index.ts",
+		},
+		outputDirs: {
+			esm: "./dist",
+		},
+	};
+	await build(options);
 })();
